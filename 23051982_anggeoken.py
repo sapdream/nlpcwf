8dd0a43fc0c5e13fa8b9d7db30e40f1d3d2cfc92
sapdream <156305094+sapdream@users.noreply.github.com>
2024-01-12 09:44:34 +0800

    Update 23051982_anggeoken.py

--- 23051982_anggeoken.py
+++ 23051982_anggeoken.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1pvObLe5sxQvVVD5M-iJsM_2U96zy6IUm
 """
 
-pip install transformers
 pip install streamlit
 
 from transformers import pipeline
